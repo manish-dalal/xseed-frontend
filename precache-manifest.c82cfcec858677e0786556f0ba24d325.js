9e7847879999e6fcc94172f75324b9c5a9fadd78
manish-dalal <manish.dalal@daffodilsw.com>
2019-11-25 02:09:51 +0530

    Updates

---
+++ precache-manifest.c82cfcec858677e0786556f0ba24d325.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3befe429b3c8cc1e0e5376b4f1bb4cb8",
+    "url": "/xseed-frontend/index.html"
+  },
+  {
+    "revision": "f7574efbefe30c830640",
+    "url": "/xseed-frontend/static/css/2.8801471a.chunk.css"
+  },
+  {
+    "revision": "7ddb029cf3c02d747ee5",
+    "url": "/xseed-frontend/static/css/main.4bb1ec7e.chunk.css"
+  },
+  {
+    "revision": "f7574efbefe30c830640",
+    "url": "/xseed-frontend/static/js/2.d3d293c1.chunk.js"
+  },
+  {
+    "revision": "7ddb029cf3c02d747ee5",
+    "url": "/xseed-frontend/static/js/main.4f8eefff.chunk.js"
+  },
+  {
+    "revision": "2ab0b5c1218db6cc80fe",
+    "url": "/xseed-frontend/static/js/runtime-main.de77c3d8.js"
+  },
+  {
+    "revision": "7c0ce41f5b47ed83de8e413ce6eed3dc",
+    "url": "/xseed-frontend/static/media/ChennaiSuperKings.7c0ce41f.jpg"
+  },
+  {
+    "revision": "fb4dc6c95e5e554a5b061ef57a345219",
+    "url": "/xseed-frontend/static/media/ChennaiSuperKingsLogo.fb4dc6c9.png"
+  },
+  {
+    "revision": "9fb08efcc8c0e93dde5216b9387d37e4",
+    "url": "/xseed-frontend/static/media/DeccanChargers.9fb08efc.jpg"
+  },
+  {
+    "revision": "fbfbc29ffcaff4fcedd6557d4916a11e",
+    "url": "/xseed-frontend/static/media/DeccanChargersLogo.fbfbc29f.png"
+  },
+  {
+    "revision": "74fc3dfdc226f94d4eed7c35b8d95e9d",
+    "url": "/xseed-frontend/static/media/DelhiDaredevils.74fc3dfd.jpg"
+  },
+  {
+    "revision": "93aa7498c0c6c206da1d2ad411e9989e",
+    "url": "/xseed-frontend/static/media/DelhiDaredevilsLogo.93aa7498.png"
+  },
+  {
+    "revision": "c016436455717ae71c8c644744cc36b0",
+    "url": "/xseed-frontend/static/media/GujaratLions.c0164364.jpg"
+  },
+  {
+    "revision": "34b0964ec4c677ed50eac4113dcf7623",
+    "url": "/xseed-frontend/static/media/GujaratLionsLogo.34b0964e.png"
+  },
+  {
+    "revision": "75ce27d4f8796f3c283dcfea8366c396",
+    "url": "/xseed-frontend/static/media/KingsPunjab.75ce27d4.jpg"
+  },
+  {
+    "revision": "72548a4a4a06a44a2d2204a93d1ae216",
+    "url": "/xseed-frontend/static/media/KingsPunjabLogo.72548a4a.png"
+  },
+  {
+    "revision": "774dcb2262969b3e00df8d578e52cbd3",
+    "url": "/xseed-frontend/static/media/KochiTuskersKerala.774dcb22.jpg"
+  },
+  {
+    "revision": "cebd62db96d2159f5cb79bb1220b3b32",
+    "url": "/xseed-frontend/static/media/KochiTuskersKeralaLogo.cebd62db.png"
+  },
+  {
+    "revision": "3e462feba72eaea0c226ddb1d53533a9",
+    "url": "/xseed-frontend/static/media/KolkataKnightRiders.3e462feb.jpg"
+  },
+  {
+    "revision": "cf1db774bf0d25febedec4c9c7020f5e",
+    "url": "/xseed-frontend/static/media/KolkataKnightRidersLogo.cf1db774.png"
+  },
+  {
+    "revision": "b99c6d04560a2650d67c16f499bb14df",
+    "url": "/xseed-frontend/static/media/MumbaiIndians.b99c6d04.jpg"
+  },
+  {
+    "revision": "354e03e0127358036082a9fd6166c6e9",
+    "url": "/xseed-frontend/static/media/MumbaiIndiansLogo.354e03e0.png"
+  },
+  {
+    "revision": "fe7ec4b4e739291f1465263693db674e",
+    "url": "/xseed-frontend/static/media/PuneWarriorsIndia.fe7ec4b4.jpg"
+  },
+  {
+    "revision": "0806d52bb603196def3ccd618e50c6e0",
+    "url": "/xseed-frontend/static/media/PuneWarriorsIndiaLogo.0806d52b.png"
+  },
+  {
+    "revision": "8581561fb2ed7c2ecdc82e541dc3af92",
+    "url": "/xseed-frontend/static/media/RajasthanRoyals.8581561f.jpg"
+  },
+  {
+    "revision": "11642aa0f30faa56bb7587f2a7a4502a",
+    "url": "/xseed-frontend/static/media/RajasthanRoyalsLogo.11642aa0.png"
+  },
+  {
+    "revision": "f2919dd75017103344959f8424b95010",
+    "url": "/xseed-frontend/static/media/RisingPuneSupergiants.f2919dd7.jpg"
+  },
+  {
+    "revision": "73e950d61b2dc9fb0af53423547c0111",
+    "url": "/xseed-frontend/static/media/RisingPuneSupergiantsLogo.73e950d6.png"
+  },
+  {
+    "revision": "d39c7623b528e5fa8aea4e60ec51bfe9",
+    "url": "/xseed-frontend/static/media/RoyalChallengersBangalore.d39c7623.png"
+  },
+  {
+    "revision": "e6df48f93e046d5816a2b1de49915cb9",
+    "url": "/xseed-frontend/static/media/RoyalChallengersBangaloreBg.e6df48f9.jpg"
+  },
+  {
+    "revision": "abeff125676130e06261abe264a408c8",
+    "url": "/xseed-frontend/static/media/SunrisersHyderabad.abeff125.png"
+  },
+  {
+    "revision": "0ba743b8bce795079ee097f5028fec5b",
+    "url": "/xseed-frontend/static/media/SunrisersHyderabadBg.0ba743b8.jpg"
+  },
+  {
+    "revision": "c9b7e9687e278de5c0df97527d4aed11",
+    "url": "/xseed-frontend/static/media/defaultBg.c9b7e968.jpg"
+  },
+  {
+    "revision": "e7257fc5616c358eccf8fcfcfd43e758",
+    "url": "/xseed-frontend/static/media/logo.e7257fc5.png"
+  }
+]);
